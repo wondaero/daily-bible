2c9d3500cba050e62278fd7f358834f5e6c9e8c7
wondaero <wondaero@nate.com>
2025-05-31 14:41:26 +0900

    원래 상태로 복귀

--- assets/js/indexedDB.js
+++ assets/js/indexedDB.js
@@ -1,4 +1,5 @@
 function IndexedDB(param){
+    this.param2 = param;
 
     function constructor(){
         // IndexedDB 연결 열기
@@ -18,9 +19,10 @@ function IndexedDB(param){
     constructor();
 
     this.query = (cmd, value, opt) => {
-        const cmds = 'crud-bomi'; //- 하이픈은 안씀, b는 backup, o는 overwrite, m은 multiUpdate, i는 initData
+        const cmds = 'crud';
 
         if(cmd.length !== 1 || cmds.indexOf(cmd) < 0) return;
+
 
         const openDB = indexedDB.open(param.dbNm, param.dbVersion);
 
@@ -65,7 +67,13 @@ function IndexedDB(param){
                             }
                         }
                     };
+
+
                 }
+
+
+
+
 
             }else if(cmd === 'u'){
                 if(!value || !value[param.key]) return;
@@ -94,7 +102,6 @@ function IndexedDB(param){
                         console.log(`ID ${value[param.key]} 데이터 없음`);
 
                         if(opt && opt.upsert === true){
-                            console.log(value);
                             const insertRequest = store.put(value);
                             insertRequest.onsuccess = function () {
                                 console.log(`ID ${value[param.key]} 데이터 등록 완료`);
@@ -128,106 +135,8 @@ function IndexedDB(param){
                 cmdRequest.onerror = function () {
                     console.error(`ID ${value} 데이터 삭제 실패`);
                 };
-            }else if(cmd === 'b'){
-                cmdRequest = store.openCursor();
-                const allData = [];
-
-                cmdRequest.onsuccess = (event) => {
-                    const cursor = event.target.result;
-                    if (cursor) {
-                        allData.push(cursor.value); // 필요한 데이터 수집
-                        cursor.continue(); // 다음으로 이동
-                    } else {
-                        // 다 모았으면 파일로 저장
-                        const json = JSON.stringify(allData, null, 2);
-                        const blob = new Blob([json], { type: 'application/json' });
-                        const url = URL.createObjectURL(blob);
-
-                        const a = document.createElement('a');
-                        a.href = url;
-                        // a.download = 'backup.json';
-                        a.download = createFileName();
-                        a.style.display = 'none';
-                        document.body.appendChild(a);
-                        a.click();
-                        a.remove();
-                        URL.revokeObjectURL(url);
-
-                        console.log('백업 완료!');
-                    }
-                };
-
-                cmdRequest.onerror = () => {
-                    console.error("커서 순회 중 오류 발생");
-                };
-            }else if(cmd === 'o'){
-
-                cmdRequest = store.openCursor();
-                const allData = [];
-
-                cmdRequest.onsuccess = (event) => {
-                    const cursor = event.target.result;
-                    if (cursor) {
-                        allData.push(cursor.value); // 필요한 데이터 수집
-                        cursor.continue(); // 다음으로 이동
-                    } else {
-                        if(opt && typeof opt.success === 'function') opt.success(allData);
-                    }
-                };
-
-                cmdRequest.onerror = () => {
-                    console.error("커서 순회 중 오류 발생");
-                };
-
-            }else if(cmd === 'i'){
-                cmdRequest = store.clear();  // 이게 전체 데이터 삭제
-
-                cmdRequest.onsuccess = () => {
-                    console.log('스토어 데이터 전체 삭제 완료');
-                    if(opt && typeof opt.success === 'function'){
-                        opt.success();
-                    }
-                    db.close();
-                };
-
-                cmdRequest.onerror = (e) => {
-                    console.error('스토어 데이터 삭제 실패', e.target.error);
-                    db.close();
-                };
-            }else if(cmd === 'm'){
-                value.forEach(item => {
-                    store.put(item); // 또는 store.add(item)
-                });
-
-                transaction.oncomplete = () => {
-                    console.log('모든 데이터 저장 완료!');
-
-                    if(opt && typeof opt.success === 'function'){
-                        opt.success();
-                    }
-                    
-                    db.close();
-                };
-
-                transaction.onerror = (event) => {
-                    console.error('트랜잭션 에러:', event.target.error);
-                };
-
             }
 
         };
-    }
-      
-    function createFileName(){
-        const now = new Date();
-
-        const pad = (n) => n.toString().padStart(2, '0');
-
-        const dateStr = `${now.getFullYear()}${pad(now.getMonth() + 1)}${pad(now.getDate())}`;
-        const timeStr = `${pad(now.getHours())}${pad(now.getMinutes())}${pad(now.getSeconds())}`;
-
-        const filename = `backup_${dateStr}_${timeStr}.json`;
-
-        return filename;
     }
 }

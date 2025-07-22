49eea03533fbae25d1f52e7f31b04018bc39d6a6
wondaero <wondaero@nate.com>
2025-07-22 22:10:26 +0900

    다운로드 경로 변경(.제거)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1255,12 +1255,10 @@ document.getElementById('downloadBtn').addEventListener('click', () => {
     if(randomInt + 1 !== +prmt) return;
 
     const a = document.createElement('a');
-    const path = '/assets/download/매일성경-debug.apk.zip'; 
-    a.href = path;
-    const splited = path.split('/');
+    a.href = '/assets/download/매일성경.zip';;
     
     a.style.display = 'none';
-    a.download = splited[splited.length - 1];
+    a.download = '';
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
